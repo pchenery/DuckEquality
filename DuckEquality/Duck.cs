10e39da6eccc2d4b202b09f8d10f322c92af3488
Peter Chenery <peter.chenery@anthonynolan.org>
2018-01-10 12:49:31 +0000

    Initial Code

---
+++ DuckEquality/Duck.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckEquality
+{
+    public class Duck
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public int WeightInGrams { get; }
+        public int AgeInMonths { get; }
+        public static Comparer<Duck> NameComparer { get; } = new NameRelationalComparer();
+
+        public Duck(string name, string type, int weightInGrams, int ageInMonths)
+        {
+            Name = name;
+            Type = type;
+            WeightInGrams = weightInGrams;
+            AgeInMonths = ageInMonths;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Duck)obj);
+        }
+        public bool Equals(Duck other)
+        {
+            return Name == other.Name &&
+                   Type == other.Type &&
+                   WeightInGrams == other.WeightInGrams &&
+                   AgeInMonths == other.AgeInMonths;
+        }
+
+        public static bool operator == (Duck left, Duck right)
+        {
+            return Equals(left, right);
+        }
+        public static bool operator != (Duck left, Duck right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = Name.GetHashCode();
+            hash = (hash * 23) + Type.GetHashCode();
+            hash = (hash * 23) + WeightInGrams.GetHashCode();
+            return hash;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, Breed {Type}, Age {AgeInMonths} weight {WeightInGrams} grams";
+        }
+
+        private sealed class NameRelationalComparer : Comparer<Duck>
+        {
+            public override int Compare(Duck x, Duck y)
+            {
+                if (ReferenceEquals(x, y)) return 0;
+                if (ReferenceEquals(null, y)) return 1;
+                if (ReferenceEquals(null, x)) return -1;
+                return x.Name.CompareTo(y.Name);
+            }
+        }
+    }
+
+}
---
+++ DuckEquality/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
+            var quackFrench = new Duck("Al Orange", "Muscovy", 200, 12);
+            var quackChinese = new Duck("Crispy", "Mandarin", 150, 10);
+            var quackFunny = new Duck("Monty", "Swedish Blue", 140, 14);
+            var quackSame = new Duck("Sir Quackalot", "Mallard", 100, 17);
+            var quackSimilar = new Duck("Sir Quickalot", "Mallard", 100, 17);
+
+            if (quack.Equals(quackSame)) { Console.WriteLine("Quack and QuackSame are equal"); }
+            if (quack == quackSame) { Console.WriteLine("Quack and QuackSame are =="); }
+            if (quack.Equals(quackSimilar)) { Console.WriteLine("These two ducks are equal"); }
+            Console.ReadLine();
+        }
+    }
+}
